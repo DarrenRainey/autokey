5a0f9826ffca54dcc52b85033d0e39e3edd9425c
DarrenRainey <darren@darrenraineys.co.uk>
2015-07-10 22:00:56 +0100

    Update extractDoc.py

--- extractDoc.py
+++ extractDoc.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Copyright (C) 2008 Chris Dekter
-
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
